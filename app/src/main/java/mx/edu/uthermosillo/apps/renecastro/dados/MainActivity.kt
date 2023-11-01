aa50c01de4a90860cd248f32a58d34050a4ed828
elrenee92 <elrenee92@gmail.com>
2023-10-31 20:36:41 -0700

    Se creo el proyecto

---
+++ app/src/main/java/mx/edu/uthermosillo/apps/renecastro/dados/MainActivity.kt
@@ -0,0 +1,39 @@
+package mx.edu.uthermosillo.apps.renecastro.dados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val caras = findViewById<TextView>(R.id.numcaras)
+        val tirar = findViewById<Button>(R.id.tirar)
+        val resultado = findViewById<TextView>(R.id.resultado)
+        val dados = findViewById<TextView>(R.id.numdados)
+
+        tirar.setOnClickListener {
+
+            val NumeroDeCaras = caras.text.toString()
+            val TextCaras = NumeroDeCaras.toInt()
+            val dadosText = dados.text.toString()
+            val numdados = dadosText.toInt()
+
+            if (numdados >=1){
+
+                val Multiplicacion = numdados * TextCaras
+                val dadoaleatoreo = ( numdados .. Multiplicacion).random()
+                resultado.text = dadoaleatoreo.toString()
+            }
+            else
+            {
+                Toast.makeText(this, "El nÃºmero de dados debe ser mayor a 0", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+}
